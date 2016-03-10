fdb15a8f66904a71461abde62172cb1d7ac3111d
sdwe4685 <sdwe4685@naver.com>
2016-03-10 18:51:01 +0900

    Create app.js

---
+++ 02/app.js
@@ -0,0 +1,51 @@
+//var str = 'hello worldd!';
+//var nick = 'my nick is \'aji\'';
+//console.log(str);
+/*
+var man = {
+  name : 'aj',
+  age : 30,
+  hobby: 'bike'
+}
+
+for(var count=0; count < 5; count++){
+
+  console.log(count)
+}
+
+function sum(x){
+    var y = 1;
+    return x+y; //return ?? ???? undefined??
+}
+
+
+console.log(sum(10));
+
+alert("alert");
+//var manName = 'aj';
+//var manAge = 30;
+*/
+//man.age
+//man.ho
+
+//console.log(x)
+/*
+for(var x=2; x<=9; x++){
+  for(var y = 1; y<=9; y++){
+    console.log(x + " X " + y + " = " + x*y);
+  }
+}
+*/
+var x, sum = 0;
+for(var y = false; y === false; ){
+ x = prompt("숫자");
+  if(x === null){
+    alert(sum);
+    y = true;
+    // break;
+  }
+  else {
+    sum += parseInt(x, 10);
+  }
+}
+alert(sum);
